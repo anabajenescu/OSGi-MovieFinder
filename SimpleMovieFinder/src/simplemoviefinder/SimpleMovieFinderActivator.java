bce5e0948f7d2ec03dae988d66e5eecae19b372a
Ana Bajenescu <bajenescu.ana@gmail.com>
2021-10-13 15:21:32 +0300

    Add Simple Movie Finder

---
+++ SimpleMovieFinder/src/simplemoviefinder/SimpleMovieFinderActivator.java
@@ -0,0 +1,44 @@
+package simplemoviefinder;
+
+import java.util.Dictionary;
+import java.util.Properties;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+import moviefinder.MovieFinder;
+
+public class SimpleMovieFinderActivator implements BundleActivator {
+
+	private static BundleContext context;
+	
+	private ServiceRegistration<?> serviceRegistration;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	public void start(BundleContext bundleContext) throws Exception {
+		System.out.println("SimpleMovieFinderActivator: start");
+		
+		MovieFinder movieFinder = new SimpleMovieFinderImpl();
+		Dictionary properties = new Properties();
+		properties.put("category", "misc"); //TODO check this
+
+		System.out.println("SimpleMovieFinderActivator: Preparing to register MovieFinder service");
+		serviceRegistration = context.registerService(MovieFinder.class.getName(), movieFinder, properties);
+		System.out.println("SimpleMovieFinderActivator: MovieFinder service registered successfully!");
+		
+		SimpleMovieFinderActivator.context = bundleContext;
+	}
+
+	public void stop(BundleContext bundleContext) throws Exception {
+		System.out.println("SimpleMovieFinderActivator: Preparing to unregister service");
+		serviceRegistration.unregister();
+		System.out.println("SimpleMovieFinderActivator: Service unregistered successfully!");
+		
+		SimpleMovieFinderActivator.context = null;
+	}
+
+}
---
+++ SimpleMovieFinder/src/simplemoviefinder/SimpleMovieFinderImpl.java
@@ -0,0 +1,28 @@
+package simplemoviefinder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import moviefinder.Movie;
+import moviefinder.MovieFinder;
+
+public class SimpleMovieFinderImpl implements MovieFinder{
+	
+	private static final List<Movie> MOVIES = populateMovies();
+
+	@Override
+	public List<Movie> findAll() {
+		System.out.println("SimpleMovieFinderImpl: Get Simple Movies");
+
+		return MOVIES;
+	}
+	
+	private static List<Movie> populateMovies() {
+		List<Movie> movies = new ArrayList<>();
+		movies.add(new Movie("The Lord of the Rings: The Fellowship of the Ring", "Peter Jackson"));
+		movies.add(new Movie("The Hobbit: An Unexpected Journey", "Peter Jackson"));
+		
+		return movies;
+	}
+
+}
